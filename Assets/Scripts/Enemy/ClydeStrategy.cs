b0a6c998f88c5b14ecee9b3150cc4fc0faa05171
SiryVovk <faoirgame@gmail.com>
2025-10-09 14:48:50 +0300

    Temp

--- Assets/Scripts/Enemy/ClydeStrategy.cs
+++ Assets/Scripts/Enemy/ClydeStrategy.cs
@@ -6,6 +6,15 @@ public class ClydeStrategy : GhostStratagySO
 {
     public override Vector2Int GetTargetPosition(PlayerMovement player, Ghost self, List<Ghost> allGhost, Field field)
     {
-        throw new System.NotImplementedException();
+        float distanse = Vector2Int.Distance(self.GetGhostPosition(), player.GetPlayerGridPosition());
+
+        if (distanse > 8)
+        {
+            return player.GetPlayerGridPosition();
+        }
+        else
+        {
+            return player.GetPlayerGridPosition();
+        }
     }
 }
